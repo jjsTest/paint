08b45e3be8b069736c5bd3cb86b2f6a51a11f9d5
jjsTest <julee00211@gmail.com>
2021-03-16 22:45:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
-  const canvasRef = createRef(); 
+  const canvasRef = useRef(null); 
+  const pos = {
+    x: -1,
+    y: -1
+  };
+
+  useEffect(() => {
+    if(canvasRef.current){
+      const ct = canvasRef.current.getContext('2d');
+      canvasRef.current.addEventListener("mousedown");
+      canvasRef.current.addEventListener("mousemove");
+      canvasRef.current.addEventListener("mouseup");
+      canvasRef.current.addEventListener("mouseout");
+    }
+  },[canvasRef.current]);
+
+  function mousedown(event){
+    ct.beginPath();
+  }
   return (
     <div className ="App">
-      <canvas ref = {canvasRef} width="500" height="300" />
+      <canvas ref = {canvasRef} 
+              style={{width="500", height="300"}}
+              onMouseDown={} />
+
     </div>
 
     // <div className="App">
@@ -26,5 +47,6 @@ function App() {
     // </div>
   );
 }
+
 
 export default App;
